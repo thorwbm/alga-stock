7c5172ec331a69c502b035cf6fe0045adcf76da3
wemerson maduro <lagoawb@gmail.com>
2021-06-12 15:45:24 -0300

    criando e analizando o fuxo

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
 
 class ClassComponent extends React.Component<{name: string}>{
+    constructor(props: any){
+        super(props);
+        console.log("construtor reched " + this.props.name);
+    }
+
     state = {
         name: 'MUNDO'
     }
+
+    componentDidMount(){
+        console.log("depois do componente montado")
+    }
+
+    componentDidUpdate(){
+        console.log(" updatando o componente ")
+    }
+    
     render(){
+        console.log("entrou no render")
         return  <div> 
                     <p> nome: {this.state.name}</p>
                     <button onClick={
-                        () => {this.setState({name:'thor'})}
+                        () => {
+                                if(this.state.name === 'thor'){ 
+                                    this.setState({name:'nao thor'})
+                                } else {
+                                    this.setState({name:'thor'})
+                                }
+                            }
                     } >click me</button>
                 </div>
     }
