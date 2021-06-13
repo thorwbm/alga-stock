0084856eb8f35d99c4b05596ae2724a29666d619
wemerson maduro <lagoawb@gmail.com>
2021-06-13 10:57:21 -0300

    limpando as regras doa aplicacao

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import TestComponent from '../TestComponent';
-// import ClassComponent from '../ClassComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <TestComponent name="thor"/>
-          {/* <ClassComponent name="mundox"/> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
