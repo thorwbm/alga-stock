f4d0e85602144117b4a079e95dcc9e768a470658
wemerson maduro <lagoawb@gmail.com>
2021-06-13 11:58:53 -0300

    adicionando o header components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import Header from '../Header';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      App
+      <Header title="Alga Stock"/>
     </div>
   );
 }
