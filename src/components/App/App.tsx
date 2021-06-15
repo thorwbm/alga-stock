fb6466b74921df16eb7a52d89bed7bfda30c5b1b
wemerson maduro <lagoawb@gmail.com>
2021-06-15 19:33:48 -0300

    limpar app e adicionar arquivos base do componete base

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Container from '../../shared/Conteiner';
 import Header from '../Header';
 import './App.css';
 
-function TestComponent () {
-  return <img width="16px" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt= "search icon"/>
-}
-
 function App() {
-
   return (
     <div className="App">
       <Header title="Alga Stock"/>
