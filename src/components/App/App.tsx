6250d240fc73f6d19b42ed29901422044b7d508a
wemerson maduro <lagoawb@gmail.com>
2021-06-15 19:11:02 -0300

    renderizacao de lista

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import Button from '../../shared/Button';
 import Container from '../../shared/Conteiner';
-import Input from '../../shared/Input';
-
 import Header from '../Header';
 import './App.css';
 
@@ -11,25 +8,13 @@ function TestComponent () {
 }
 
 function App() {
-  const [street, setStreet] = useState('')
 
   return (
     <div className="App">
       <Header title="Alga Stock"/>
       
       <Container>
-        <Button
-            onClick={() => window.alert('uiiiiu')}
-            appendIcon={<TestComponent/>}
-        >
-          alert
-        </Button>
-        <Input 
-          label="Street" 
-          placeholder="digite o nome da sua rua"
-          value = {street}
-          onChange={e => setStreet(e.target.value)}
-        />
+        
       </Container>
     </div>
   );
