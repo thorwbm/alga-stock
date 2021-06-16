d324ca263b6e5b269fdca468b1d7b1a41ba8c8b5
wemerson maduro <lagoawb@gmail.com>
2021-06-15 21:05:09 -0300

    etilizacao da tabela scss

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
-// import './Table.scss'
+import './Table.scss'
 
 const Table = () => {
-    return  <table>
+    return  <table className="AppTable">
                 <thead>
                     <tr>
                         <th>Produto</th>
                         <th>preço</th>
-                        <th>estoque</th>
+                        <th className="right">estoque</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>cookie</td>
                         <td>$1.35</td>
-                        <td>23</td>
+                        <td className="right">23</td>
                     </tr>
                     <tr>
                         <td>Leite 1L</td>
                         <td>$2.35</td>
-                        <td>10</td>
+                        <td className="right">10</td>
                     </tr>
                 </tbody>
             </table>
