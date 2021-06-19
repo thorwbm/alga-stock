9c1b8423000f4b2d74853a38b420febd6d8dca79
wemerson maduro <lagoawb@gmail.com>
2021-06-19 15:18:41 -0300

    fix componente tabela

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -3,34 +3,85 @@ import Products from './Table.mockdata'
 import './Table.scss'
 
 
-const Headers = [
+const Headers: TableHeader[] = [
+    {key: 'id', value: '#'},
     {key: 'name', value: 'Product'},
-    {key: 'price', value: 'Price'},
-    {key: 'actions', value: 'Acations'},
-    {key: 'stock', value: 'Avaliable Stock'}
+    {key: 'price', value: 'Price', right: true},
+    {key: 'stock', value: 'Avaliable Stock', right: true}
 ]
+declare interface TableHeader {
+    key: string
+    value: string
+    right?: boolean    
+}
+
+type IndexedHeaders = {
+    [key: string] : TableHeader
+}
+
+type OrganizedItem = {
+    [key: string] : any
+}
+
+function organizeData(data: any[], Headers: TableHeader[]): 
+ [OrganizedItem[], IndexedHeaders] {
+    const IndexedHeaders: IndexedHeaders = {}
+
+    Headers.forEach(header => {
+        IndexedHeaders[header.key] = {
+            ...header
+        }
+    })
+
+    const headerKeysInOorder = Object.keys(IndexedHeaders)
+
+    const organizedData = data.map(item => {
+        const organizedItem: OrganizedItem = {}
+
+        headerKeysInOorder.forEach(key => {
+            organizedItem[key] = item[key]
+        })
+
+        organizedItem.$original = item
+        return organizedItem
+    })
+
+    return [organizedData,IndexedHeaders]
+}
 
 const Table = () => {
+
+    const [organizedData, indexedHeaders] = organizeData(Products,Headers)
+
     return  <table className="AppTable">
                 <thead>
                     <tr>
-                        {Headers.map(header =>  <td key={header.key}>
-                                                    {header.value}
-                                                </td>
-                                          )}
+                        {Headers.map(header =>  
+                            <th  className={header.right ? 'right' : '' } key={header.key}>
+                                {header.value}
+                            </th>
+                        )}
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        Products.map(product =>
-                            <tr>
-                                <td>{product.name}</td>
-                                <td>{product.price}</td>
-                                <td className="right">{product.stock}</td>
+                        organizedData.map((row, i) => {
+                            return <tr key = {i}>
+                                {
+                                    Object
+                                    .keys(row)
+                                    .map((item, i) => {
+
+                                        return  item === '$original' ? null :
+                                        <td  key = {row.$original.id + i}
+                                                    className={indexedHeaders[item].right ? 'right' : ''}> 
+                                                    {row[item]}
+                                                </td>}
+                                        )
+                                }
                             </tr>
-                        )
+                        })
                     }
-                    
                 </tbody>
             </table>
 }
