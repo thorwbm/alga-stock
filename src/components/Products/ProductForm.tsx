4245aa94b8ad300400be8c4dc89da2fe3b7cc27a
wemerson maduro <lagoawb@gmail.com>
2021-06-22 19:59:46 -0300

    corrigindo

--- src/components/Products/ProductForm.tsx
+++ src/components/Products/ProductForm.tsx
@@ -2,31 +2,10 @@ import React from 'react'
 
 
 import Form from '../../shared/Form'
-import Input from '../../shared/Input'
-import Button from '../../shared/Button'
 
 const ProductForm = () => {
-    return <Form onSubmit={console.log}>
-         <Input
-            label= "Name"
-            placeholder= "ex. Cookies"
-          />
-          <Input
-            label= "valor"
-            type="number"
-            step="0.01"
-            min="0"
-            placeholder= "ex. 1.25"
-          />
-          <Input
-            label= "stock"
-            type="number"
-            min="0"
-            placeholder= "ex. 10"
-          />
-          <Button>
-            enviar
-          </Button>
+    return <Form>
+
     </Form>
 }
 
