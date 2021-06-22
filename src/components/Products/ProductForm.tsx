58e12725ee00bc99bee5d2e8305612b59ce69060
wemerson maduro <lagoawb@gmail.com>
2021-06-22 20:46:27 -0300

    adiconando o componente productForm

--- src/components/Products/ProductForm.tsx
+++ src/components/Products/ProductForm.tsx
@@ -1,12 +1,57 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 
 import Form from '../../shared/Form'
+import Input from '../../shared/Input'
+import Button from '../../shared/Button'
+
+const initialFormState = {
+    name: '',
+    price: '',
+    stock: ''
+}
 
 const ProductForm = () => {
-    return <Form>
+    const [form, setForm] = useState(initialFormState)
 
-    </Form>
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {value, name} = event.target
+
+        setForm({
+            ...form, [name]:value
+        })
+    }
+
+    return <Form title="lista de produtos" onSubmit={() => console.log(form)}>
+    <Input
+      onChange = {handleInputChange}
+      name="name"
+      label= "Name"
+      placeholder= "ex. Cookies"
+      required
+    />
+    <Input
+      onChange = {handleInputChange}
+      name="price"
+      label= "price"
+      type="number"
+      step="0.01"
+      min="0"
+      placeholder= "ex. 1.25"
+      required
+    />
+    <Input
+      onChange = {handleInputChange}
+      name="stock"
+      label= "stock"
+      type="number"
+      min="0"
+      placeholder= "ex. 10"
+      required
+    />
+    <Button>
+      Submmit
+    </Button>
+  </Form>
 }
 
 export default ProductForm
