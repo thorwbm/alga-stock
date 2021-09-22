375ba7e9551f1c1c772632f3dc574809e21a26ce
wemerson maduro <lagoawb@gmail.com>
2021-09-22 20:21:06 -0300

    update product schemas

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-
-import http from './Utils/http'
+import http from './utils/http';
 
 http.get('/posts')
+http.get('/posts/1')
+http.get('https://viacep.com.br/ws/29140160/json/')
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
