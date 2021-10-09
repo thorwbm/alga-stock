83e31c485690c6af2412e5971b6983207b19cc1d
wemerson maduro <lagoawb@gmail.com>
2021-10-09 11:48:42 -0300

    adicionando reducer, action, store

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import http from './utils/http';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import http from './utils/http'
 
-http.get('/posts')
-http.get('/posts/1')
-http.get('https://viacep.com.br/ws/29140160/json/')
+import { Provider } from 'react-redux'
+import store from './redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
